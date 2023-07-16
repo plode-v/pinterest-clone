6131d3ffdcbc847cb598cb8479bf07318ba8bd05
Plode <84858705+plode-v@users.noreply.github.com>
2023-07-16 18:48:46 +0700

    Update authentication

--- app/components/LoginButton.tsx
+++ app/components/LoginButton.tsx
@@ -1,21 +1,22 @@
+'use client'
+import Image from 'next/image';
 import React from 'react'
 import { useSession, signIn, signOut } from "next-auth/react";
 
-const LoginButton = () => {
+const LoginButton: React.FC = () => {
 
     const { data: session } = useSession()
     if (session) {
+        console.log(session.user)
         return (
             <>
-                Signed in as {session.user.email} <br />
-                <button onClick={() => signOut()}>Sign Out</button>
+            <Image src='/man.png' alt='user-profile' width={50} height={50} className='cursor-pointer p-3 hover:bg-gray-200 rounded-full' onClick={() => signOut()} />
             </>
         )
     }
     return (
         <>
-            Not signed in <br />
-            <button onClick={() => signIn()}>Sign in</button>
+            <button onClick={() => signIn()} className='font-[600]'>Sign in</button>
         </>
     )
 }
