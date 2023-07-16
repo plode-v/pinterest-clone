ce746641b25b7b51b67549de30cf2480a72a209c
Plode <84858705+plode-v@users.noreply.github.com>
2023-07-16 14:55:08 +0700

    Update header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,7 +7,7 @@ import { HiSearch, HiBell, HiChat, HiChevronDown } from "react-icons/hi"
 const Header = () => {
 
   return (
-	<div className='flex p-4 items-center'>
+	<div className='flex p-4 items-center w-screen'>
 		<Image src='/logo.png' alt='Logo' width={50} height={50} className='hover:bg-gray-100 p-3 rounded-full cursor-pointer'/>
 		<div className='gap-7 flex mr-7'>
 			<button className='bg-black text-white py-3 px-4 rounded-full font-[600]'>Home</button>
@@ -21,11 +21,17 @@ const Header = () => {
 			<HiSearch className='text-gray-600' />
 			<input type="text" placeholder="search" className='bg-[#e9e9e9] active:border-0 w-full focus:outline-none' />
 		</div>
-		<div className='flex w-1/6 justify-evenly items-center'>
-			<HiBell className='text-gray-600 text-[50px] cursor-pointer p-3 hover:bg-gray-200 rounded-full' />
-			<HiChat className='text-gray-600 text-[50px] cursor-pointer p-3 hover:bg-gray-200 rounded-full' />
+		<div className='flex w-1/4 justify-evenly items-center'>
+			<div className='w-[50px]'>
+				<HiBell className='text-gray-600 text-[50px] cursor-pointer p-3 hover:bg-gray-200 rounded-full' />
+			</div>
+			<div className='w-[50px]'>
+				<HiChat className='text-gray-600 text-[50px] cursor-pointer p-3 hover:bg-gray-200 rounded-full' />
+			</div>
 			<Image src='/man.png' alt='user-image' width={50} height={50} className='cursor-pointer p-3 hover:bg-gray-200 rounded-full' />
-			<HiChevronDown className='cursor-pointer hover:bg-gray-100 text-[30px] p-1 rounded-full' />
+			<div className='w-[30px]'>
+				<HiChevronDown className='cursor-pointer hover:bg-gray-100 text-[30px] rounded-full' />
+			</div>
 		</div>
 	</div>
 )
